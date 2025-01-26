6e6b7232b6c7bb89e866b3d90ac307db45fa2397
A-Mikayelyan <a.mikayelyyan@gmail.com>
2025-01-26 20:31:38 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import React from 'react'
-import ToDoList2 from './ToDoList2';
+import React from 'react';
+import ToDoList2 from './ToDoList2'; 
 
-function App(){
-
-  
-
-
-  return(
-    <>
-
-    <ToDoList2/>
-
-    
-       
-    
-    </>
-  )
-
+function App() {
+  return (
+    <div>
+      <ToDoList2 /> 
+    </div>
+  );
 }
 
-
-
-
-
-  
-
-  
-
-  
-
-
-export default App 
+export default App;
