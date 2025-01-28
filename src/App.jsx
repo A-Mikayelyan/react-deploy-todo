f5149c870d4604c665c37892f11dc0f07b3d5709
Artavazd <a.mikayelyyan@gmail.com>
2025-01-28 15:42:40 +0400

    css-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import React from 'react'
-import ToDoList2 from './ToDoList2';
+import React from 'react';
+import ToDoList2 from './ToDoList2'; // Correct import
 
-function App(){
-
-  
-
-
-  return(
-    <>
-
-    <ToDoList/>
-
-    
-       
-    
-    </>
-  )
-
+function App() {
+  return (
+    <div>
+      <ToDoList2 /> {/* Correct component name */}
+    </div>
+  );
 }
 
-
-
-
-
-  
-
-  
-
-  
-
-
-export default App 
+export default App;
